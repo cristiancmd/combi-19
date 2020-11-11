4aeb187aad74135013079eb309c9981030c2e1ce
cristian <cristian.cmd@gmail.com>
2020-11-10 21:20:00 -0300

    crear modificar y eliminar Ciudad

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  resources :tests
   resources :chofers
   resources :trips
   resources :users
   resources :buses
   resources :routes
   resources :cities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root "cities#index"
+
+  
+
+  #get "cities" => "cities#create" # button_to
+  #post "cities" => "cities#edit" # y link_to
+  #post "cities" => "cities#create" # button_to
+  #post "cities/new" => "cities#new" # y link_to
+
 end
