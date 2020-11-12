780cbd5b79a921cc29b0df0ec12a98941bc50888
Hernan Kleinubing <hernan.k97@gmail.com>
2020-11-12 14:33:46 -0300

    Agregado las funcionalidades

--- app/controllers/chofers_controller.rb
+++ app/controllers/chofers_controller.rb
@@ -2,19 +2,44 @@ class ChofersController < ApplicationController
 	def index
 		@chofers = Chofer.all()
 	def new
-
+		@chofer = Chofer.new()
 	end
 	def show
-
+		@chofer = Chofer.find(params[:id])
 	end
-	def edit
-
+	def create
+		parametros = params.require(:chofer).permit(:nombre, :apellido, :dni, :inicio_actividad)	
+		@chofer = Chofer.new(parametros)
+	  	respond_to do |format|
+	  		if @chofer.save
+	  			format.html { redirect_to chofers_path, notice: 'El chofer se actualizo correctamente.' }
+	  		else
+	  			format.html { render :index }
+	  		end
+	  	end
+  	end
+  	def edit
+  		@chofer = Chofer.find(params[:id])
 	end
+
+    def update
+    	@chofer = Chofer.find(params[:id])
+    	params.require(:chofer).permit(:nombre, :apellido, :dni, :inicio_actividad)
+
+	  	respond_to do |format|
+	  		if @chofer.update(nombre: params[:chofer][:nombre])
+	  			format.html { redirect_to chofers_path, notice: 'El chofer se actualizo correctamente.' }
+	  		else
+	  			format.html { render :edit }
+	  		end
+	  	end
+	  	end
+	 end
+	
 	def destroy
-
+		chofer = Chofer.find(params[:id])
+		chofer.destroy
+		redirect_to chofers_path
 	end
-	def update
-		
+
   	end
-  	end
-end
