d281d5ccb4286dbf3dea094f52c14a58843f65c4
Hernan Kleinubing <hernan.k97@gmail.com>
2020-11-13 11:03:35 -0300

    Agregada validación

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -15,12 +15,16 @@ class RoutesController < ApplicationController
 	end
 	def create
     	@route = Route.new(route_params)
-		
-		respond_to do |format|
-			if @route.save
-				format.html { redirect_to routes_path , notice: 'Ruta creada exitosamente.' }
-			else
-				format.html { render :new }
+    	
+    	if Route.where(initial_city: @route.initial_city, destination_city: @route.destination_city).exists?
+    		redirect_to routes_path , alert: 'La ruta ya existe'
+    	else
+			respond_to do |format|
+				if @route.save
+					format.html { redirect_to routes_path , notice: 'Ruta creada exitosamente.' }
+				else
+					format.html { render :new }
+				end
 			end
 		end
 	end
