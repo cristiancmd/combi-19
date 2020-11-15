13d31c261a94ab58b4f61aba83f0dda8de20dd59
cristian <cristian.cmd@gmail.com>
2020-11-15 14:50:01 -0300

    correccion de validaciones en rutas y ciudad

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,4 @@
 class City < ApplicationRecord
-	
+	 validates :nombre, presence: true
+	 validates :nombre, uniqueness: {message:": La ciudad ya existe!", case_sensitive: false }
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,16 @@
 class Route < ApplicationRecord
 	belongs_to :initial_city, class_name: 'City'
 	belongs_to :destination_city, class_name: 'City'
+
+	validates :initial_city, presence: true
+	validates :destination_city, presence: true
+	validates :check_equality , presence: true
+
+	
+
+	
+
+	def check_equality
+	    initial_city.id != destination_city.id
+	end
 end
