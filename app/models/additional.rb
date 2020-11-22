44aab24e5146e3d130f265b8e03467ac076bcd39
Hernan Kleinubing <hernan.k97@gmail.com>
2020-11-21 21:51:06 -0300

    Precio y Stock de adicionales no puede ser negativo

--- app/models/additional.rb
+++ app/models/additional.rb
@@ -2,4 +2,6 @@ class Additional < ApplicationRecord
 
 	 validates :nombre, presence: true
 	 validates :nombre, uniqueness: {message:": El extra ya existe!", case_sensitive: false }
+	 validates :precio, :numericality => { :greater_than_or_equal_to => 0 }
+	 validates :stock, :numericality => { :greater_than_or_equal_to => 0 }
 end
