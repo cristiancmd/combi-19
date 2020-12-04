b3e1f2a5557c72422caa816e37afff7cf6202da4
cristian <cristian.cmd@gmail.com>
2020-12-03 22:13:05 -0300

    corregido consistencia de busqueda magiaca en user y admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Trip.destroy_all
+#Trip.destroy_all
 
-Route.destroy_all
-City.destroy_all
-Chofer.destroy_all
-User.destroy_all
+#Route.destroy_all
+#City.destroy_all
+#Chofer.destroy_all
+#User.destroy_all
 
 Admin.destroy_all
 Bus.destroy_all
